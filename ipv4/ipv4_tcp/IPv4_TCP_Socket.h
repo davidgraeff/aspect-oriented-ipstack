341ab4070772ac24140a9d214d379c93556c70b4
chb <chb@857cdeed-beef-0310-bd7e-d983e88953ba>
2011-02-25 14:47:30 +0000

    CiAO IP-Stack: moved tcp api function implemenation to .cpp file
    git-svn-id: https://www4.informatik.uni-erlangen.de:8088/i4ciao/ciaoos/trunk@1383 857cdeed-beef-0310-bd7e-d983e88953ba

--- ipv4/ipv4_tcp/IPv4_TCP_Socket.h
+++ ipv4/ipv4_tcp/IPv4_TCP_Socket.h
@@ -29,19 +29,7 @@ class IPv4_TCP_Socket : public IPv4_Socket, public TCP_Socket {
     return false;
   }
   
-  void recv_loop(){
-    while(TCP_Socket::waiting_for_input()) {
-      IPv4_Packet* packet = IPv4_Socket::read(); //non-blocking read
-      if(packet != 0){
-        unsigned len = packet->get_total_len() - packet->get_ihl()*4;
-        TCP_Segment* segment = (TCP_Segment*) packet->get_data();
-        TCP_Socket::input(segment, len);
-      }
-      else{
-        TCP_Socket::input(0, 0);
-      }
-    }
-  }
+  void recv_loop();
   
   // explicit join-points: affected by 'IPv4_TCP_Receive.ah' aspect.
   bool bind() { return false; } // register a new client socket at Demux
@@ -52,84 +40,15 @@ class IPv4_TCP_Socket : public IPv4_Socket, public TCP_Socket {
     TCP_Socket::set_network_header_offset(IPV4_MIN_HEADER_SIZE);
   }
 
-  bool connect(){
-    if(IPv4_Socket::get_dst_ipv4_addr() == 0 || IPv4_Socket::get_src_ipv4_addr() == 0){
-      return false; //no (valid) dst_ipv4_addr set
-    }
-    if(TCP_Socket::isClosed()){
-      //only allow connecting in CLOSED state
-      TCP_Socket::set_sport(TCP_Segment::UNUSED_PORT); //reset source port
-      if(bind()){
-        TCP_Socket::setMTU(IPv4_Socket::getMTU());
-        TCP_Socket::connect();
-        recv_loop();
-        return TCP_Socket::isEstablished();
-      }
-    }
-    return false;
-  }
+  bool connect();
   
-  bool close(){ //full close (no half-close supported)
-    TCP_Socket::close();
-    recv_loop();
-    unbind();
-    return TCP_Socket::isClosed();
-  }
+  bool close(); //full close (no half-close supported)
   
-  bool send(const void* data, unsigned datasize){
-    if( isEstablished() || isCloseWait() ){
-      TCP_Socket::addSendData(data, datasize);
-      recv_loop();
-      return true;
-    }
-    return false;
-  }
+  bool send(const void* data, unsigned datasize);
   
-  int receive(void* buffer, unsigned buffer_len){
-    if( isEstablished() || isCloseWait() ){
-      //zero out MSB, because return value is an int!
-      const unsigned msb = 1 << ((8*sizeof(unsigned))-1);
-      buffer_len &= ~msb;
-      TCP_Socket::receive(buffer, buffer_len);
-      recv_loop();
-      unsigned recv_len = TCP_Socket::get_application_recv_len();
-      if( (recv_len == 0) && isCloseWait() ){
-        return -2;
-      }
-      return (int)recv_len;
-    }
-    return -1;
-  }
+  int receive(void* buffer, unsigned buffer_len);
 
-  int poll(unsigned msec){
-    if( isEstablished() || isCloseWait() ){
-      IPv4_Packet* packet = IPv4_Socket::read(); //non-blocking read
-      if(packet == 0){
-        //no gratuitous packets received, yet
-        bool timeout_reached = TCP_Socket::block((UInt32)msec); //wait for max. msec
-        if(timeout_reached == false) {
-          packet = IPv4_Socket::read(); //check for new packet, since timeout was not reached
-        }
-      }
-      if(packet != 0){
-        do {
-          //process all gratuitous packets that have arrived
-          unsigned len = packet->get_total_len() - packet->get_ihl()*4;
-          TCP_Segment* segment = (TCP_Segment*) packet->get_data();
-          TCP_Socket::input(segment, len);
-          packet = IPv4_Socket::read();
-        } while(packet != 0);
-        updateHistory(); //cleanup packets which are not used anymore
-        processACK(); //send ACK if necessary
-      }
-
-      //return amount of received data bytes (payload) while polling
-      //zero out MSB, because return value is an int!
-      const unsigned msb = 1 << ((8*sizeof(unsigned))-1);
-      return (int) (TCP_Socket::getRecvBytes() & ~msb);
-    }
-    return -1;
-  }
+  int poll(unsigned msec);
   
 };
 
