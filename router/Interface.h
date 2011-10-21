b334a433e6ba26f03101def36bbc38caf0580ade
chb <chb@857cdeed-beef-0310-bd7e-d983e88953ba>
2011-10-21 12:33:11 +0000

    IP-Stack: simplified (and optimized) network device driver binding
    git-svn-id: https://www4.informatik.uni-erlangen.de:8088/i4ciao/ciaoos/trunk@1534 857cdeed-beef-0310-bd7e-d983e88953ba

--- router/Interface.h
+++ router/Interface.h
@@ -1,19 +1,11 @@
 #ifndef __INTERFACE__
 #define __INTERFACE__
 
-namespace hw {
-namespace hal {
-class NetworkDevice; //Forward declaration solves include-cycle problem
-}}
-using hw::hal::NetworkDevice;
-
-
 namespace ipstack {
 
-class Interface{
+class Interface {
   private:
   Interface* next_interface; //linked list
-  NetworkDevice* device; //the device driver (for delegation)
   //IPv4 Attributes to be sliced in by IPv4-Aspect
   
   public:
@@ -22,20 +14,20 @@ class Interface{
          SLIP = 3 };
   
   Interface* getNext() { return next_interface; }
-  void setNetworkDevice(NetworkDevice* dev){ device = dev; }
   
-  //delegation to NetworkDevice
-  const char* getName();
-  unsigned char getType();
-  const unsigned char* getAddress();
-  unsigned getMTU();
+  //these functions have to be delegated to network device abstraction
+  // -> performed by aspect Interface.ah
+  const char* getName() { return 0; }
+  unsigned char getType() { return 0; }
+  const unsigned char* getAddress() { return 0; }
+  unsigned getMTU() { return 0; }
   
-  bool hasTransmitterHardwareChecksumming();
-  bool hasReceiverHardwareChecksumming();
+  bool hasTransmitterHardwareChecksumming() { return false; }
+  bool hasReceiverHardwareChecksumming() { return false; }
   
-  void send(const void* frame, unsigned framesize);
-  void send(const void* data, unsigned datasize, bool tcp, unsigned ip_hdl);
-  bool hasBeenSent(const void* frame);
+  void send(const void* frame, unsigned framesize) {}
+  void send(const void* data, unsigned datasize, bool tcp, unsigned ip_hdl) {}
+  bool hasBeenSent(const void* frame){ return true; }
 
 };
 
