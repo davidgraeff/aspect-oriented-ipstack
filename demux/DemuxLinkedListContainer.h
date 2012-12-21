c32037e54352b557b0bd78bac3284b677900bbe3
graeff <graeff@857cdeed-beef-0310-bd7e-d983e88953ba>
2012-12-21 11:13:44 +0000

    Ipstack: Add Demux linked-list class. This is used by all sockets that register for receiving traffic
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4ciao/ciaoos/trunk@1712 857cdeed-beef-0310-bd7e-d983e88953ba

---
+++ demux/DemuxLinkedListContainer.h
@@ -0,0 +1,38 @@
+// This file is part of CiAO/IP.
+//
+// CiAO/IP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CiAO/IP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CiAO/IP.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2012 David Gräff
+
+#pragma once
+/**
+ * Purpose: Add a next pointer to a class so that sockets etc can be organised in a linked-list fashion
+ * within the demux class
+ */
+
+namespace ipstack {
+
+template <class T>
+class DemuxLinkedList {
+friend class Demux;
+private:
+	T* next;
+public:
+	DemuxLinkedList() : next(0) {}
+	T* getNext() { return next; }
+	void setNext(T* sock) { next = sock; }
+	
+};
+
+} // namespace ipstack
