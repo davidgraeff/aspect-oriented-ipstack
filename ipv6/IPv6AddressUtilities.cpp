877f1e4880f13736356524945fc6ee8d9cb32986
graeff <graeff@857cdeed-beef-0310-bd7e-d983e88953ba>
2013-01-10 16:42:59 +0000

    ipstack/ipv6: utility functions for converting binary ipv6 addresses and their string representations and for determining ipv6 address scopes and related functionality.
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4ciao/ciaoos/trunk@1753 857cdeed-beef-0310-bd7e-d983e88953ba

---
+++ ipv6/IPv6AddressUtilities.cpp
@@ -0,0 +1,292 @@
+#include "IPv6AddressUtilities.h"
+namespace ipstack {
+namespace IPV6AddressScope {
+UInt8 getIPv6AddressScopePrefixLength(ipstack::IPV6AddressScope::IPv6_ADDRESS_SCOPE s)
+{
+	switch (s) {
+		case IPV6_SCOPE_LINKLOCAL:
+			return 64;
+		case IPV6_SCOPE_GLOBAL_UNICAST:
+			return 3;
+		case IPV6_SCOPE_MULTICAST:
+			return 8;
+		case IPV6_SCOPE_UNIQUE_LOCAL_UNICAST:
+			return 7;
+		default:
+			return 0;
+	};
+}
+IPv6_ADDRESS_SCOPE getIPv6AddressScope(const ipv6addr& addr)
+{
+	// 2000::/3 global unqiue addresses;
+	if ((addr.ipaddrB8[0] & 0x30) == 1)
+		return IPV6_SCOPE_GLOBAL_UNICAST;
+
+	// ff00::/8 multicast
+	if (addr.ipaddrB8[0] == 0xff)
+		return IPV6_SCOPE_MULTICAST;
+
+	// fe80::/64 link local
+	if ((addr.ipaddrB8[0] == 0xfe) && (addr.ipaddrB8[1] == 0x80))
+		return IPV6_SCOPE_LINKLOCAL;
+
+	// fc00::/7 privat addresses (fc... global unqiue; fd... site unqiue)
+	if ((addr.ipaddrB8[0] == 0xfc) || (addr.ipaddrB8[0] == 0xfd))
+		return IPV6_SCOPE_UNIQUE_LOCAL_UNICAST;
+
+	// first 10 bytes == 0
+	if ((addr.ipaddrB8[0] == 0) && (addr.ipaddrB8[1] == 0) && (addr.ipaddrB8[2] == 0) && (addr.ipaddrB8[3] == 0)
+			&& (addr.ipaddrB8[4] == 0) && (addr.ipaddrB8[5] == 0) && (addr.ipaddrB8[6] == 0) && (addr.ipaddrB8[7] == 0)
+			&& (addr.ipaddrB8[8] == 0) && (addr.ipaddrB8[9] == 0)) {
+		// 0:0:0:0:0:ffff::/96 IPv4 mapped;
+		if ((addr.ipaddrB8[10] == 0xff) && (addr.ipaddrB8[11] == 0xff))
+			return IPV6_SCOPE_GLOBAL_UNICAST;
+
+		// first 15 bytes == 0
+		if ((addr.ipaddrB8[10] == 0) && (addr.ipaddrB8[12] == 0) && (addr.ipaddrB8[13] == 0) && (addr.ipaddrB8[14] == 0)) {
+			// :0 non defined address
+			if (addr.ipaddrB8[15] == 0)
+				return IPV6_SCOPE_NONDEFINED;
+			// :1 loopback
+			else if (addr.ipaddrB8[15] == 1)
+				return IPV6_SCOPE_LOOPBACK;
+		}
+	}
+
+	return IPV6_SCOPE_NOT_RECOGNICED;
+}
+}
+void copy_ipv6_addr(const ipv6addr& src, ipv6addr& dest)
+{
+// 	dest.ipaddrB8[0] = src.ipaddrB8[0];
+// 	dest.ipaddrB8[1] = src.ipaddrB8[1];
+// 	dest.ipaddrB8[2] = src.ipaddrB8[2];
+// 	dest.ipaddrB8[3] = src.ipaddrB8[3];
+// 	dest.ipaddrB8[4] = src.ipaddrB8[4];
+// 	dest.ipaddrB8[5] = src.ipaddrB8[5];
+// 	dest.ipaddrB8[6] = src.ipaddrB8[6];
+// 	dest.ipaddrB8[7] = src.ipaddrB8[7];
+// 	dest.ipaddrB8[8] = src.ipaddrB8[8];
+// 	dest.ipaddrB8[9] = src.ipaddrB8[9];
+// 	dest.ipaddrB8[10] = src.ipaddrB8[10];
+// 	dest.ipaddrB8[11] = src.ipaddrB8[11];
+// 	dest.ipaddrB8[12] = src.ipaddrB8[12];
+// 	dest.ipaddrB8[13] = src.ipaddrB8[13];
+// 	dest.ipaddrB8[14] = src.ipaddrB8[14];
+// 	dest.ipaddrB8[15] = src.ipaddrB8[15];
+	memcpy(dest.ipaddrB8, src.ipaddrB8, 16);
+}
+void get_unspecified_ipv6_address(ipv6addr& dest)
+{
+	dest.ipaddrB8[0] = 0;
+	dest.ipaddrB8[1] = 0;
+	dest.ipaddrB8[2] = 0;
+	dest.ipaddrB8[3] = 0;
+	dest.ipaddrB8[4] = 0;
+	dest.ipaddrB8[5] = 0;
+	dest.ipaddrB8[6] = 0;
+	dest.ipaddrB8[7] = 0;
+	dest.ipaddrB8[8] = 0;
+	dest.ipaddrB8[9] = 0;
+	dest.ipaddrB8[10] = 0;
+	dest.ipaddrB8[11] = 0;
+	dest.ipaddrB8[12] = 0;
+	dest.ipaddrB8[13] = 0;
+	dest.ipaddrB8[14] = 0;
+	dest.ipaddrB8[15] = 0;
+}
+bool is_not_unspecified_ipv6_address(const ipv6addr& dest)
+{
+	char u = 0;
+	for (UInt8 i = 0; i < 16; ++i)
+		u |= dest.ipaddrB8[i];
+	return (bool)u;
+}
+
+void solicitedNode_multicast_addr(const ipstack::ipv6addr& src_ip_addr, ipv6addr& solicitedNode) {
+	solicitedNode.ipaddrB8[0] = 0xff;
+	solicitedNode.ipaddrB8[1] = 0x02;
+	solicitedNode.ipaddrB8[2] = 0;
+	solicitedNode.ipaddrB8[3] = 0;
+	solicitedNode.ipaddrB8[4] = 0;
+	solicitedNode.ipaddrB8[5] = 0;
+	solicitedNode.ipaddrB8[6] = 0;
+	solicitedNode.ipaddrB8[7] = 0;
+	solicitedNode.ipaddrB8[8] = 0;
+	solicitedNode.ipaddrB8[9] = 0;
+	solicitedNode.ipaddrB8[10] = 0;
+	solicitedNode.ipaddrB8[11] = 0x01;
+	solicitedNode.ipaddrB8[12] = 0xff;
+	solicitedNode.ipaddrB8[13] = src_ip_addr.ipaddrB8[13];
+	solicitedNode.ipaddrB8[14] = src_ip_addr.ipaddrB8[14];
+	solicitedNode.ipaddrB8[15] = src_ip_addr.ipaddrB8[15];
+}
+
+UInt8 numberFromAscii(char c)
+{
+	switch (c) {
+		case '0':
+			return 0;
+		case '1':
+			return 1;
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		case '5':
+			return 5;
+		case '6':
+			return 6;
+		case '7':
+			return 7;
+		case '8':
+			return 8;
+		case '9':
+			return 9;
+		case 'a':
+			return 10;
+		case 'A':
+			return 10;
+		case 'b':
+			return 11;
+		case 'B':
+			return 11;
+		case 'c':
+			return 12;
+		case 'C':
+			return 12;
+		case 'd':
+			return 13;
+		case 'D':
+			return 13;
+		case 'e':
+			return 14;
+		case 'E':
+			return 14;
+		case 'f':
+			return 15;
+		case 'F':
+			return 15;
+		default:
+			return 0;
+	};
+}
+UInt8 numberToAscii(UInt8 digit)
+{
+	switch (digit) {
+		case 0:
+			return '0';
+		case 1:
+			return '1';
+		case 2:
+			return '2';
+		case 3:
+			return '3';
+		case 4:
+			return '4';
+		case 5:
+			return '5';
+		case 6:
+			return '6';
+		case 7:
+			return '7';
+		case 8:
+			return '8';
+		case 9:
+			return '9';
+		case 10:
+			return 'a';
+		case 11:
+			return 'b';
+		case 12:
+			return 'c';
+		case 13:
+			return 'd';
+		case 14:
+			return 'e';
+		case 15:
+			return 'f';
+		default:
+			return ' ';
+	};
+}
+bool compare_ipv6_addr(const ipv6addr& addr, const ipv6addr& addrprefix, UInt8 prefixlen)
+{
+	for (UInt8 block = 0; block < 16; ++block) {
+		// entire char has to be the same
+		if (prefixlen >= 8) {
+			if (addr.ipaddrB8[block] != addrprefix.ipaddrB8[block]) {
+				return false;
+			}
+			prefixlen -= 8;
+			if (prefixlen == 0)
+				return true;
+		} else { // only part of the char has to be the same
+			/**
+			 * Calculate the mask:
+			 * 1.) 2^prefixlen -1: If prefixlen is 5 for example: we have 00011111: Five 1's.
+			 * 2.) Because we want the 1's starting on the LSB, because we compare from "left to right"
+			 *     we use 7-prefixlen (exmpl: 00000111) and invert the result (exmpl: 11111000)
+			 */
+			UInt8 mask = ~((2 ^(7 - prefixlen)) - 1);
+			if ((addr.ipaddrB8[block] & mask) != (addrprefix.ipaddrB8[block] & mask)) {
+				return false;
+			}
+			return true;
+		}
+	}
+	return true;
+}
+bool compare_ipv6_addr(const ipv6addr& addr, const ipv6addr& addr2)
+{
+	return (memcmp(addr.ipaddrB8, addr2.ipaddrB8, 16) == 0);
+}
+bool parse_ipv6_addr(const char* addrstr, ipv6addr& ipaddr)
+{
+	UInt8 blockIndex = 0;
+	char* currentCharPtr = (char*)addrstr;
+	while (blockIndex < 16) {
+		UInt8 blockdata[4] = {0};
+		UInt8 position = 0;
+		//printf("block ");
+		while (*currentCharPtr != ':') {
+			//printf ("%c", *currentCharPtr);
+			blockdata[position] = numberFromAscii(*currentCharPtr);
+			++currentCharPtr;
+			if (++position > 3)
+				break;
+		}
+		ipaddr.ipaddrB8[blockIndex * 2 + 0] = (blockdata[1]) | (blockdata[0] << 4);
+		ipaddr.ipaddrB8[blockIndex * 2 + 1] = (blockdata[3]) | (blockdata[2] << 4);
+		if (*currentCharPtr == 0)
+			break;
+		++currentCharPtr;
+		//printf(" -> (%u %u) (%u %u)\n", blockIndex*2+1, ipaddr.ipaddrB8[blockIndex*2+1], blockIndex*2+0, ipaddr.ipaddrB8[blockIndex*2+0]);
+		++blockIndex;
+	}
+	if (blockIndex != 16)
+		return 0;
+	return 1;
+}
+void ipv6_addr_toString(const ipv6addr& ipaddr, char* addrstr)
+{
+	// A block consists of 4 digits and a ":". An IPv6 address has 8 blocks.
+	UInt8 addstrPosOffset = 0;
+	for (UInt8 block = 0; block < 8; ++block) {
+		// first digit of "2001:"
+		addrstr[addstrPosOffset + 0] = numberToAscii(ipaddr.ipaddrB8[block * 2 + 0] >> 4); // higher 4 bits
+		// second digit of "2001:"
+		addrstr[addstrPosOffset + 1] = numberToAscii(ipaddr.ipaddrB8[block * 2 + 0] & 0x0f); // lower 4 bits
+		// third digit of "2001:"
+		addrstr[addstrPosOffset + 2] = numberToAscii(ipaddr.ipaddrB8[block * 2 + 1] >> 4); // higher 4 bits
+		// forth digit of "2001:"
+		addrstr[addstrPosOffset + 3] = numberToAscii(ipaddr.ipaddrB8[block * 2 + 1] & 0x0f); // lower 4 bits
+		// the colon
+		if (block < 7)
+			addrstr[addstrPosOffset + 4] = ':';
+		addstrPosOffset += 5;
+	}
+}
+}
---
+++ ipv6/IPv6AddressUtilities.h
@@ -0,0 +1,100 @@
+// This file is part of CiAO/IPv6-Extension.
+//
+// CiAO/IP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CiAO/IP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CiAO/IPv6-Extension.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2012 David Gräff
+
+#pragma once
+/*
+ * Purpose: IPv6 address conversions from and to different formats and IPv6-Address scope. These utility methods
+ * should be optimized out if not used by the compiler and do not need seperate aspects.
+ */
+
+#include "util/types.h"
+#include <string.h>
+
+namespace ipstack {
+
+// IPv6 address representations
+union ipv6addr {
+	// byte[0] is the most "left", byte[15] the most "right"
+	// Example: "2001:0db8:0000:08d3:0000:8a2e:0070:7344" -> byte[0]==0x20, byte[15]==0x44
+	UInt8 ipaddrB8[16]; // representation: byte array
+// 	UInt16 ipaddrB16[8]; // representation: byte array
+};
+
+namespace IPV6AddressState {
+enum IPv6_ADDRESS_STATE {
+	IPV6_ADDRESS_PRELIMINARY,
+	IPV6_ADDRESS_OK
+};
+}
+namespace IPV6AddressScope {
+enum IPv6_ADDRESS_SCOPE {
+	IPV6_SCOPE_NONDEFINED, // :0 the not defined IP
+	IPV6_SCOPE_LOOPBACK,   // :1 loopback IP
+	IPV6_SCOPE_LINKLOCAL,  // fe80::/64
+	IPV6_SCOPE_UNIQUE_LOCAL_UNICAST, // fc00::/7 privat addresses (fc... global unqiue; fd... site unqiue)
+	IPV6_SCOPE_MULTICAST, // ff00::/8 multicast
+	IPV6_SCOPE_GLOBAL_UNICAST, // 0:0:0:0:0:ffff::/96 IPv4 mapped; 2000::/3 global unqiue addresses; there are more prefixes for this scope but not fully defined so far
+	IPV6_SCOPE_NOT_RECOGNICED
+};
+UInt8 getIPv6AddressScopePrefixLength(IPv6_ADDRESS_SCOPE s);
+IPv6_ADDRESS_SCOPE getIPv6AddressScope(const ipstack::ipv6addr& addr);
+}
+
+/**
+  * Build the corresponding  multicast address for a link local address
+  * src_ip_addr: The link local address
+  * solicitedNode: The destination multicast address
+  */
+void solicitedNode_multicast_addr(const ipstack::ipv6addr& src_ip_addr, ipv6addr& solicitedNode);
+
+/**
+ * Copy an IPv6 address
+ */
+void copy_ipv6_addr(const ipstack::ipv6addr& src, ipstack::ipv6addr& dest);
+
+/**
+ * Get unspecified address (::/128)
+ */
+void get_unspecified_ipv6_address(ipstack::ipv6addr& dest);
+bool is_not_unspecified_ipv6_address(const ipstack::ipv6addr& dest);
+
+/**
+ * If addr matches the addrprefix with prefixlen return true
+ */
+bool compare_ipv6_addr(const ipstack::ipv6addr& addr, const ipstack::ipv6addr& addrprefix, UInt8 prefixlen);
+/**
+ * A fast way to compare, if all bytes have to be compared.
+ */
+bool compare_ipv6_addr(const ipstack::ipv6addr& addr, const ipstack::ipv6addr& addr2);
+
+/**
+ * Parse a textual ipv6 address representation like "2001:0db8:0000:08d3:0000:8a2e:0070:7344" into the native representation.
+ * A short variant is also possible, where leading 0's are left out, like "2001:db8::08d3::8a2e:70:7344".
+ * @param addrstr Textual address representation
+ * @param ipaddr Resulting native ipv6 represenation
+ * @return Return true if successful otherwise false. ipaddr is not altered if the parsing was not successful
+ */
+bool parse_ipv6_addr(const char* addrstr, ipstack::ipv6addr& ipaddr);
+
+/**
+ * Parse a native representation into the textual ipv6 address representation like "2001:0db8:0000:08d3:0000:8a2e:0070:7344".
+ * @param ipaddr Native ipv6 represenation
+ * @param addrstr Memory for the textual address representation. 39 characters are generated.
+ */
+void ipv6_addr_toString(const ipstack::ipv6addr& ipaddr, char* addrstr);
+
+} //namespace ipstack
