0735d44e8e073fff2bb61608a11fc7a6564cd578
graeff <graeff@857cdeed-beef-0310-bd7e-d983e88953ba>
2013-01-10 16:55:28 +0000

    ipstack/ipv6: add multicast receiving/sending support (without mld protocol, which is part of icmpv6)
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4ciao/ciaoos/trunk@1757 857cdeed-beef-0310-bd7e-d983e88953ba

---
+++ ipv6/multicast/MulticastEntry.h
@@ -0,0 +1,44 @@
+ // This file is part of CiAO/IP.
+//
+// CiAO/IP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CiAO/IP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CiAO/IP.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2012 David Gräff
+
+#pragma once
+
+#include "ipstack/ipv6/IPv6AddressUtilities.h"
+#include "util/types.h"
+
+namespace ipstack {
+/**
+ * An IPv6->Link layer address cache. The link layer address have to be smaller or equal to 6 bytes in size.
+ * Without further influences by aspects this implementation is synchronous: The process flow is blocked
+ * while looking up a link layer address.
+ */
+struct MulticastGroupEntry {
+	enum { ID = 240};
+	unsigned char id;
+	ipv6addr ipv6;
+	UInt8 state;
+	/**
+	 * Use this after getting a free memory block from AddressMemory.
+	 * This will mark the memory block as beeing used.
+	 */
+	void reserve() {
+		id = ID;
+	}
+} __attribute__((packed));
+#define MulticastGroupEntryType MulticastGroupEntry, MulticastGroupEntry::ID
+
+}
