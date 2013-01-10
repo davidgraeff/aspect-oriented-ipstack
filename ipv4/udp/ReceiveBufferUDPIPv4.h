6e4d0971fe5c519f367de5f73ec71e1a1499de4a
graeff <graeff@857cdeed-beef-0310-bd7e-d983e88953ba>
2013-01-10 16:22:39 +0000

    ipstack/ipv4: udp support: port of the ipv4/udp support to the refactored codebase
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4ciao/ciaoos/trunk@1744 857cdeed-beef-0310-bd7e-d983e88953ba

---
+++ ipv4/udp/ReceiveBufferUDPIPv4.h
@@ -0,0 +1,67 @@
+// This file is part of CiAO/IP.
+//
+// CiAO/IP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CiAO/IP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CiAO/IP.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Copyright (C) 2012 David Gräff
+
+#pragma once
+
+#include "util/types.h"
+#include "ipstack/util/Mempool.h"
+#include "ipstack/ReceiveBuffer.h"
+#include <string.h> //for memcpy
+
+namespace ipstack {
+
+class ReceiveBufferUDPIPv4 : public ReceiveBuffer {
+public:
+	struct RemoteInfo {
+		UInt8 id;
+		UInt16 remoteport;
+		UInt32 ipv4;
+	};
+	/**
+	 * Create a receive buffer and return it.
+	 * Changes the ReceiveBuffer list-head pointer if neccessary.
+	 */
+	static ReceiveBufferUDPIPv4* createReceiveBufferUDPIPv4(Mempool* mempool, void*  payload, UInt16 payloadsize, UInt16 remoteport, UInt32 ipv4) {
+		ReceiveBufferUDPIPv4* r = (ReceiveBufferUDPIPv4*)ReceiveBuffer::createReceiveBuffer(mempool, payload, payloadsize + sizeof(RemoteInfo));
+		if (!r)
+			return 0;
+		
+		r->m_receivedSize = payloadsize;
+		char* d = r->m_dataStart;
+		d+= payloadsize;
+		RemoteInfo* remoteinfo = (RemoteInfo*)d;
+		remoteinfo->id = '4';
+		remoteinfo->remoteport = remoteport;
+		remoteinfo->ipv4 = ipv4;
+		
+		return r;
+	}
+	
+	static ReceiveBufferUDPIPv4* cast(ReceiveBuffer* b) {
+		if (((ReceiveBufferUDPIPv4*)b)->getRemoteInfo()->id != '4')
+			return 0;
+		return (ReceiveBufferUDPIPv4*)b;
+	}
+	
+	RemoteInfo* getRemoteInfo() {
+		char* d = m_dataStart;
+		d+= m_receivedSize;
+		return (RemoteInfo*)d;
+	}
+};
+
+} // namespace ipstack
