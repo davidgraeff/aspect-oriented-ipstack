33dfa90c07762eb19ddd90fc5a648f8c1499935c
graeff <graeff@857cdeed-beef-0310-bd7e-d983e88953ba>
2013-01-22 13:02:20 +0000

    IPStack/Sendbuffer: Add Interface pointer to the sendbuffer. This adds 4 bytes (32Bit plattform) memory footprint per sendbuffer but removes dozens of lines of code; Add sendbuffer protect and sendbuffer debug aspects. If you enable protection you can fairly do anything wrong with a sendbuffer, usefull for development code.
    git-svn-id: https://www4.informatik.uni-erlangen.de/i4ciao/ciaoos/trunk@1784 857cdeed-beef-0310-bd7e-d983e88953ba

--- SendBuffer.h
+++ SendBuffer.h
@@ -25,38 +25,67 @@
 namespace ipstack
 {
 
+/**
+  * A sendbuffer is used for all sending activity within the tcp/ip stack. It is manipulated and filled
+  * by different aspects of the network stack layers. The size for the buffer has to be known beforehand
+  * to allocate the memory. YOu may use different methods to write to a sendbuffer. See the documentation
+  * at Socket_Send_Slice.ah.
+  */
 class SendBuffer
 {
-	private:
-// 		char m_dataStart[]; // memory block starts here (but we have to add the size of this class for a memory pointer where data can be written)
 	public:
-		static SendBuffer* createSendBuffer(Mempool* mempool, UInt16Opt requestedSize) {
+		/**
+		  * Create a new SendBuffer object and allocate memory for it. If not enough memory is available 0
+		  * is returned. You should provide the destination interface. It is recommend to use the convenience
+		  * methods of yout socket instead of a raw SendBuffer directly.
+		  */
+		static SendBuffer* createInstance(Mempool* mempool, UInt16Opt requestedSize, Interface* interface) {
 			SendBuffer* r = (SendBuffer*)mempool->alloc(requestedSize + sizeof(SendBuffer));
 			if (!r)
 				return 0;
-			r->m_mempool = mempool;
+// 			r->m_mempool = mempool;
 			r->m_memsize = requestedSize;
+			r->m_state = WritingState;
+			r->m_interface = interface;
 			r->initStartPointer();
 			return r;
 		}
 		
-		// Initialize the data pointer
-		// This is "aspect safe": even if aspects add other data members to this class, we will never overwrite those
+		/**
+		  * We use a trick here to determine where to write the user data. We compute the the size of the class
+		  * and determine the end of the object in the memory. This has the advantage that other aspects may
+		  * add data members to this class and we will still begin with the user data after those statically
+		  * added member variables.
+		  */
 		void initStartPointer() {
 			data = getDataStart();
+		}
+		inline void* getDataStart() {
+			return this + sizeof(SendBuffer);
+		}
+		
+		/**
+		  * For debugging purposes you may associate a string with this sendbuffer. You have to enable
+		  * the SendBuffer Debuging feature for an actual output. If not enabled all mark methods in the
+		  * code should be optimised out by the compiler (only use const char strings!).
+		  * @param str The string to be used for debug messages.
+		  */
+		inline void mark(const char* str) {
+			
 		}
 
 		/**
 		 * Put your data here.
 		 * Example:
-		 * SendBuffer* buffer = socket.requestSendBuffer(12);
-		 * memcpy(buffer->data, "test", 4);
+		 * SendBuffer* buffer = socket.requestSendBuffer(4);
+		 * memcpy(buffer->getDataPointer(), "test", 4);
 		 * OR
-		 * (int32)(buffer->data[0]) = 1472984;
-		 * Do not change the pointer address yourself!
+		 * *((int32*)buffer->getDataPointer()) = 1472984;
 		 * Always call writtenToDataPointer after writting to this memory location!
 		 */
-		void* data;
+		void* getDataPointer() {
+			return data;
+		}
 
 		/**
 		 * Use this method to increment the data pointer after writing to it directly
@@ -71,8 +100,6 @@ class SendBuffer
 		 * of directly write to the data pointer in incremental steps.
 		 */
 		void write(const void* newdata, UInt16Opt length) {
-			if (!m_memsize) return;
-
 			UInt16Opt availableLength = getRemainingSize();
 			if (availableLength < length)
 				length = availableLength;
@@ -81,12 +108,33 @@ class SendBuffer
 			data = (char*)data + length;
 		}
 
-		void makeInvalid() {
-			m_memsize = 0;
-		}
-
-		bool isValid() {
-			return (m_memsize != 0);
+		/**
+		  * After allocating a SendBuffer it is in the
+		  * "Writing-State". After calling send(..) with the buffer it is in the
+		  * "Transmitted-State". If an aspect fails on the buffer it will set it
+		  * to the "Invalid-State".
+		  *
+		  * You may only write to a buffer if it is in the "Writing-State". Calling
+		  * send(..) is also only a valid action in this state. A buffer should never
+		  * be free'd while in the "Transmitted-State" without calling hasBeenSend()
+		  * before. Use the socket API for correct usage (socket::free)!
+		  *
+		  * If you write an aspect that intercept send(..) (to delay packets etc), you
+		  * may use the "AboutToBeTransmitted-State" to mark a buffer as ready to
+		  * be send.
+		  */
+		enum {InvalidState= 0x00, WritingState = 0x01, AboutToBeTransmittedState= 0x02, TransmittedState = 0x04, ResolveLinkLayerStateOption= 0x10};
+		inline void setState(UInt8 s) { m_state = s; }
+		inline UInt8 getState() { return m_state;}
+		inline UInt8 getStateWithoutOptions() { return m_state & 0x0f;} // lower bits only
+		
+		/**
+		  * A sendbuffer may be recycled if no negative effects can occur anymore.
+		  * E.g. after the network driver has successfully send the content of this
+		  * buffer (DMA finished).
+		  */
+		void recycle() {
+			m_state = WritingState;
 		}
 
 		/**
@@ -94,48 +142,30 @@ class SendBuffer
 		 * Cache this value!
 		 * */
 		UInt16Opt getRemainingSize() {
-			if (!this->m_memsize) return 0;
 			return m_memsize - ((char*) data - (char*) getDataStart());
 		}
 		UInt16Opt getSize() {
 			return m_memsize;
 		}
-		inline void* getDataStart() {
-			return this + sizeof(SendBuffer);
+		/**
+		* Return true if the sendbuffer has been send already. This will also return true if the generation of the sendbuffer
+		* failed and the sendbuffer therefore never have been relayed to the network buffer. Therefore do not
+		* call this method with failed-to-generate sendbuffers!
+		*/
+		bool hasBeenSend() {
+			return m_interface->hasBeenSent(getDataStart());
 		}
-
-	public: //TODO private
-		Mempool* m_mempool;
+		inline Interface* getInterface() { return m_interface; }
+		void setInterface(Interface* i) { m_interface = i; }
+	private:
+		explicit SendBuffer() {} // private constructor
+		SendBuffer(const SendBuffer& s) {} // private copy constructor
+		
+// 		Mempool* m_mempool;
 		UInt16Opt m_memsize; // set to  user requested size
-};
-
-/**
- * Some times it is neccary to carry the interface inline to ask a SendBuffer if it has been send already. This is the case for all
- * management sockets.
- */
-class SendBufferWithInterface
-{
-public:
-	static SendBufferWithInterface* createSendBuffer(Mempool* mempool, UInt16Opt requestedSize, Interface* interface) {
-		SendBufferWithInterface* r = (SendBufferWithInterface*)mempool->alloc(requestedSize + sizeof(SendBufferWithInterface));
-		if (!r)
-			return 0;
-		r->sendbuffer.m_mempool = mempool;
-		r->sendbuffer.m_memsize = requestedSize;
-		r->sendbuffer.initStartPointer();
-		r->interface = interface;
-		return r;
-	}
-	/**
-	  * Return true if the sendbuffer has been send already. This will also return true if the generation of the sendbuffer
-	  * failed (isValid()==false) and the sendbuffer therefore never have been relayed to the network buffer. Therefore do not
-	  * call this method with failed-to-generate sendbuffers!
-	  */
-	bool hasBeenSend() {
-		return !sendbuffer.isValid() && interface->hasBeenSent(sendbuffer.getDataStart());
-	}
-	Interface* interface;
-	SendBuffer sendbuffer;
+		UInt8 m_state;
+		Interface* m_interface;
+		void* data;
 };
 
 } // namespace ipstack
