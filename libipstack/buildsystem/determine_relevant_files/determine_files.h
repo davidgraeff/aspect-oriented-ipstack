7a11fe87493bbfa532a86a15cc31740599c7c1fe
davidgraeff <david.graeff@web.de>
2013-08-26 15:36:08 +0200

    determine_relevant_files: Output definitions as #define's now instead of compiler -D flags. Improve argument handling. If no -d is set, we do not output definitions at all. If no -o is set, we do not output relevant files

--- libipstack/buildsystem/determine_relevant_files/determine_files.h
+++ libipstack/buildsystem/determine_relevant_files/determine_files.h
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2013 David Graeff
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE REGENTS AND CONTRIBUTORS, INC. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
 #include "picojson.h"
@@ -7,18 +31,52 @@
 #include <set>
 #include <iostream>
 
+/**
+ * Output files separated by spaces that are relevant for the selected feature-set.
+ * If all_features is set, the feature-set is ignored and all files are relevant.
+ */
 class FeatureToFiles {
 public:
+	/**
+	 * Output files separated by spaces that are relevant for the selected feature-set.
+	 * If all_features is set, the feature-set is ignored and all files are relevant.
+	 */
 	FeatureToFiles(const std::string& basedir, const picojson::value::object& obj,
 				   const std::set<std::string>& kconfig_enabled,
 				   std::ostream* output_files_stream, bool all_features);
+	/**
+	 * Return true if the feature-set to files transformation has been successfull.
+	 * A parse error or non-expected data types may lead to an error and false
+	 * is returned.
+	 */
 	bool success();
 private:
+	/**
+	 * Return the string of the map at position "name". Return an empty string
+	 * if the map does not contain that key.
+	 */
 	std::string getMapString(const picojson::value::object& obj, std::string name);
+	/**
+	 * Return a list of files either via the "files" key of the obj map or via
+	 * the "file" key. An empty list is returned if neither of those keys is existing.
+	 */
 	std::list<std::string> get_files(const picojson::value::object& obj);
-	bool value(const picojson::value& value);
+	/**
+	 * Parse a json object. The FeatureToFiles json objects are those with keys
+	 * like those: name, vname, file, files, subdir, comp.
+	 * Nodes are traversed recursively.
+	 */
 	bool node(const picojson::value::object& obj);
+	/**
+	 * A "comp" element of a node is parsed via this method.
+	 * Usually a component contains several (sub-)nodes.
+	 */
 	bool component(const picojson::value::array& obj);
+	/**
+	 * Return true if the dependency given by the depends string
+	 * is met. kconfig_enabled is used for this. The depends string
+	 * may contain "not", "and", "or" and "&feature" expressions.
+	 */
 	bool checkDepends(const std::string depends);
 	
 	bool msuccess;
@@ -26,7 +84,11 @@ private:
 	std::ostream* output_files_stream;
 	bool all_features;
 	int in;
-	
+
+	/**
+	 * The directory class supports cd(subdir) and up() methods,
+	 * the current directory can be accessed by "dir".
+	 */
 	class Dir {
 	public:
 		Dir(std::string dir) {
--- libipstack/buildsystem/determine_relevant_files/main.cpp
+++ libipstack/buildsystem/determine_relevant_files/main.cpp
@@ -1,18 +1,41 @@
 /*
-* Copyright 2013 David Graeff
-* BSD License
-*/
+ * Copyright 2013 David Graeff
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE REGENTS AND CONTRIBUTORS, INC. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /**
 * This program reads a kconfig output file (usually .config) and a
 * file that contains the relations between activated features and files
 * that belong to a feature. The output is a list of used files, depending
 * on the selected features. The output is either on stdout or to a file.
+* Additionally all configuration variables of .config can be written to
+* to a c file as #define's.
 * 
 * Arguments are:
+* -b base_directory (may be ommitted, "" is used then)
 * -o output_filename (may be omitted to output to stdout)
+* -d output_definitions_filename (may be set to "" to output to stdout)
 * -k kconfig_output_filename (usually .config)
-* -f feature_to_files_relation_filename (json format)
+* -f feature_to_files_relation_filename (json format, may be omitted if -d is set)
 * -a (select all features, -k is not neccessary and ignored)
 */
 
@@ -23,20 +46,23 @@
 #include <getopt.h>
 #include <iostream>     // std::ios, std::istream, std::cout
 #include <fstream>      // std::filebuf
+#include <sstream>      // std::stringstream
 
 void usage(char** argv) {
 	fprintf(stderr,
 	"Any of the following modes may be combined (if non-exclusive options are set).\n"
-	"Mode OUTPUT DEFINITIONS: %s [-d output_file_definitions] -k kconfig_output_filename \n"
-	"	If you omit output_file_definitions the standard output is used.\n"
+	"Mode OUTPUT DEFINITIONS: [-d output_file_definitions] -k kconfig_output_filename \n"
+	"	If you set output_file_definitions to \"\" the standard output is used.\n"
 	"	The kconfig_output_filename is the output of the kconfig tool (usually .config).\n\n"
-	"Mode OUTPUT ALL FILES: %s [-o output_file] -b base_directory -f feature_to_files_relation_filename -a\n"
-	"	The feature_to_files_relation_filename is a json file. The syntax is explained in readme_familymodel.md\n",
+	"Mode OUTPUT ALL FILES: -o output_file [-b base_directory] -f feature_to_files_relation_filename -a\n"
+	"	You may set output_file to \"\" to use the standard output.\n"
+	"	You may optionally set base_directory as a prefix for all files.\n"
+	"	The feature_to_files_relation_filename is a json file. The syntax is explained in readme_familymodel.md\n"
 	"	The kconfig selection is ignored and all files are selected.\n\n"
-	"Mode OUTPUT FILES: %s [-o output_file] -b base_directory -k kconfig_output_filename -f feature_to_files_relation_filename\n"
+	"Mode OUTPUT FILES: -o output_file [-b base_directory] -f feature_to_files_relation_filename -k kconfig_output_filename\n"
 	"	The kconfig_output_filename is the output of the kconfig tool (usually .config).\n"
-	"	The feature_to_files_relation_filename is a json file. The syntax is explained in readme_familymodel.md\n",
-		argv[0]);
+	"	It is used to determine the files to ouput in contrast to OUTPUT ALL FILES mode.\n"
+	);
 }
 
 int main(int argc, char** argv) {
@@ -46,24 +72,31 @@ int main(int argc, char** argv) {
 	std::string base_directory;
 	std::string featureToFilesRelationfile;
 	bool all_features = false;
-	bool only_definitions = true;
+	bool definitions = false;
+	bool featureFiles = false;
 
 	// parse options
 	int opt;
 	while ((opt = getopt(argc, argv, "b:o:d:k:f:a")) != -1) {
 			switch (opt) {
 			case 'o':
-				output_files_file.open (optarg,std::ios::out);
-				if (!output_files_file.is_open()) {
-					std::cerr << "Could not open output file for write!\n";
-					exit(EXIT_FAILURE);
+				featureFiles = true;
+				if (strlen(optarg)>0) {
+					output_files_file.open (optarg,std::ios::out);
+					if (!output_files_file.is_open()) {
+						std::cerr << "Could not open output file for write!\n";
+						exit(EXIT_FAILURE);
+					}
 				}
 				break;
 			case 'd':
-				output_definitions_file.open (optarg,std::ios::out);
-				if (!output_definitions_file.is_open()) {
-					std::cerr << "Could not open output file for write!\n";
-					exit(EXIT_FAILURE);
+				definitions = true;
+				if (strlen(optarg)>0) {
+					output_definitions_file.open (optarg,std::ios::out);
+					if (!output_definitions_file.is_open()) {
+						std::cerr << "Could not open output file for write!\n";
+						exit(EXIT_FAILURE);
+					}
 				}
 				break;
 			case 'k':
@@ -73,7 +106,6 @@ int main(int argc, char** argv) {
 				base_directory = std::string(optarg);
 				break;
 			case 'f':
-				only_definitions = false;
 				featureToFilesRelationfile = std::string(optarg);
 				break;
 			case 'a':
@@ -86,31 +118,29 @@ int main(int argc, char** argv) {
 		}
 	
 	if (kconfigfile.size() && all_features) {
-		std::cerr << "kconfig_output file and all-features set. I'm confused now!\n";
+		std::cerr << "kconfig_output file and all-features set. all-features overwrite kconfig_output!\n\n";
+	}
+	
+	if (!kconfigfile.size() && definitions) {
+		std::cerr << "kconfig_output file not set and definitions expected. I'm confused now!\n\n";
 		usage(argv);
 		exit(EXIT_FAILURE);
 	}
 	
 	if (!kconfigfile.size() && !all_features) {
-		std::cerr << "kconfig_output file not set!\n";
+		std::cerr << "kconfig_output file not set!\n\n";
 		usage(argv);
 		exit(EXIT_FAILURE);
 	}
 
-	if (!featureToFilesRelationfile.size()) {
-		std::cerr << "feature_to_files_relation file not set!\n";
-		usage(argv);
-		exit(EXIT_FAILURE);
-	}
-	
-	if (!base_directory.size()) {
-		std::cerr << "base directory not set!\n";
+	if (!featureToFilesRelationfile.size() && (!definitions || featureFiles)) {
+		std::cerr << "feature_to_files_relation file not set and also no definitions wanted. No output!\n\n";
 		usage(argv);
 		exit(EXIT_FAILURE);
 	}
 	
 	if (!output_files_file.is_open() && !output_definitions_file.is_open()) {
-		std::cerr << "You can't output files and definitions to standard output. Call the application twice or use files!\n";
+		std::cerr << "You can't output files and definitions to standard output. Call the application twice or use files!\n\n";
 		usage(argv);
 		exit(EXIT_FAILURE);
 	}
@@ -121,22 +151,28 @@ int main(int argc, char** argv) {
 	
 	// read kconfig file
 	kconfigparser kparser;
-	if (!all_features && !kparser.load_kconfig_file(kconfigfile)) {
+	if ((definitions || !all_features) && !kparser.load_kconfig_file(kconfigfile)) {
 		std::cerr << "Failed to parse kconfig output file!\n";
 		exit(EXIT_FAILURE);
 	}
 	
-	if (!all_features) {
+	if (definitions) {
 		for (auto i = kparser.kconfig_keyvalue.begin(); i!=kparser.kconfig_keyvalue.end();++i) {
 			const std::string& key = i->first;
 			const std::string& value = i->second;
-// 			std::cerr << "def " << "-D" << key << "="<< value << ";";
-			output_definitions_stream << "-D" << key << "="<< value << ";";
+// 			double x ;
+// 			std::stringstream ss;
+// 			ss << value;
+// 			ss >> x;
+// 			if (ss.fail()) // no number
+// 				output_definitions_stream << "#define " << key << " \"" << value << "\"" << std::endl;
+// 			else // number
+			output_definitions_stream << "#define " << key << " " << value << std::endl;
 		}
 	}
-		
+
 	int res = 0;
-	if (!only_definitions) {
+	if (featureFiles) {
 		// read feature_to_files_relation file
 		std::filebuf fb;
 		if (!fb.open (featureToFilesRelationfile.c_str(),std::ios::in)) {
