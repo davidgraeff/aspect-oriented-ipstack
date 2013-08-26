2766ac03516830e04c615fbff64ff4a9c786ff82
davidgraeff <david.graeff@web.de>
2013-08-26 16:11:42 +0200

    Remove more ciao dependencies. Fix reconfigure buildsystem problem.

--- libipstack/buildsystem/kconfig.cmake
+++ libipstack/buildsystem/kconfig.cmake
@@ -6,6 +6,15 @@ if(NOT DEFINED KCONFIG_EXECUTABLE OR NOT EXISTS "${KCONFIG_EXECUTABLE}")
 	message(FATAL_ERROR "KConfig has to be build first!")
 endif()
 
+If (DEFINED Skip_if_existing AND EXISTS "${KCONFIG_RESULT_FILE}")
+	return()
+endif()
+
+if (NOT EXISTS "${KCONFIG_RESULT_FILE}")
+	file(READ "${KCONFIG_PRESET_CONFIGURATION_FILE}" TEMP)
+	file(WRITE ${KCONFIG_RESULT_FILE} ${TEMP})
+endif()
+
 # Call kconfig with ipstack.fm with the working directory where .config is located
 MESSAGE(STATUS "Run kconfig: ${KCONFIG_EXECUTABLE} ${KCONFIG_FEATUREMODEL}")
 execute_process(COMMAND "${KCONFIG_EXECUTABLE}" "${KCONFIG_FEATUREMODEL}"
